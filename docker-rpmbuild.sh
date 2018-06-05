da998333919e55c53e34b454e963b15686ee774b
Mikko Rauhala <mikko@rauhala.net>
2018-06-05 14:30:29 +0300

    Initial version

---
+++ docker-rpmbuild.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Test if proxy is needed
+wget -q -t 1 --spider www.google.com
+if [ $? -ne 0 ]; then
+    echo Using proxy http://wwwproxy.fmi.fi:8080
+    export http_proxy=http://wwwproxy.fmi.fi:8080
+    export https_proxy=http://wwwproxy.fmi.fi:8080
+    sudo sh -c 'echo proxy=http://wwwproxy.fmi.fi:8080 >> /etc/yum.conf'
+fi
+
+mkdir -p $HOME/rpmbuild/RPMS/x86_64
+createrepo $HOME/rpmbuild/RPMS/x86_64
+sudo yum repolist
+sudo yum --disablerepo="*" --enablerepo="local" list available
+
+cd $HOME/rpmbuild/SOURCES/
+git clone $1
+REPO=$(ls -1)
+tar zcvf $REPO.tar.gz --exclude-vcs $REPO
+sudo yum-builddep -y $REPO/*.spec
+rpmbuild -ba $REPO/*.spec
+createrepo $HOME/rpmbuild/RPMS/x86_64
